6b12a733885e5a627e0b90d6591264baa14562c0
MattC <matthew@versi.design>
2026-05-21 11:22:18 +1000

    Update maretti.js

--- maretti.js
+++ maretti.js
@@ -1901,7 +1901,7 @@ svg.style.display = "block";
                 baseShiftXFactor: 0.05
               }
             : {
-                baseShiftY: isMobileOurWinesMap ? 140 : 80
+                baseShiftY: isMobileOurWinesMap ? 140 : 140
               })
         : {}
     });
